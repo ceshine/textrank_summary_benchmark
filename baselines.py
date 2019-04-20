3f03271501a1aac591e7ffd25020c15536c98b8d
CeShine Lee <shuanck@gmail.com>
2019-04-20 20:22:14 +0800

    Baselines

---
+++ baselines.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+
+from tqdm import tqdm
+from rouge import Rouge
+import numpy as np
+from prettyprinter import pprint
+from summa.preprocessing.textcleaner import clean_text_by_sentences
+
+RESULTS_DIR = Path("results/")
+RESULTS_DIR.mkdir(exist_ok=True)
+
+DATA_DIR = Path("data/")
+
+
+def head(sentences, ratio):
+    length = max(len(sentences) * ratio, 1)
+    return sentences[:int(length)]
+
+
+def random(sentences, ratio):
+    length = max(int(len(sentences) * ratio), 1)
+    return np.random.choice(np.array(sentences), size=length)
+
+
+BASELINES = {
+    "random": random,
+    "head": head
+}
+
+
+def cnndm_inference():
+    """Summary generation for the CNN/DailyMail dataset"""
+    for func_name, func in BASELINES.items():
+        print("Processing \"{}\"".format(func_name))
+        print("=" * 20)
+        with open(str(DATA_DIR / "cnndm" / "test.txt.src")) as fin:
+            with open(str(RESULTS_DIR / "cnndm_baseline_{}.pred".format(func_name)), "w") as fout:
+                for line in tqdm(fin.readlines()):
+                    sentences = clean_text_by_sentences(line, "english")
+                    result = " ".join(
+                        [x.text for x in func(sentences, ratio=0.1)])
+                    fout.write(result + "\n")
+
+
+def cnndm_eval():
+    """Evaluation for the CNN/DailyMail dataset"""
+    for func_name in BASELINES:
+        print("Evaluating \"{}\"".format(func_name))
+        print("=" * 20)
+        with open(str(RESULTS_DIR / "cnndm_baseline_{}.pred".format(func_name))) as fin:
+            predictions = fin.read().split("\n")[:-1]
+        with open(str(DATA_DIR / "cnndm" / "test.txt.tgt.tagged")) as fin:
+            references = fin.read().replace("<t> ", "").replace(
+                "</t> ", "").split("\n")[:-1]
+        assert all([len(x) > 0 for x in predictions])
+        scores = Rouge().get_scores(predictions, references, avg=True)
+        pprint(scores)
+
+
+if __name__ == "__main__":
+    cnndm_inference()
+    cnndm_eval()
